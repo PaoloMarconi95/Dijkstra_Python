30a9b34907bc0a801979b165e6ba77f2181bd359
PaoloMarconi95 <Internet0Git>
2021-01-20 17:16:33 +0100

    Optimus Path Improved

--- main.py
+++ main.py
@@ -17,17 +17,21 @@ def main():
 # Custom preparation of the graph. Future release will allow graphic interface
 def prepare_graph():
     nodes = [0, 1, 2, 3, 4, 5]
+
     for node in nodes:
         nodes[node] = Node(node)
+
     nodes[0].add_neighbour(nodes[1], 4, weight_matrix, undirected_graph)
+    nodes[0].add_neighbour(nodes[3], 1, weight_matrix, undirected_graph)
     nodes[0].add_neighbour(nodes[2], 3, weight_matrix, undirected_graph)
     nodes[1].add_neighbour(nodes[2], 1, weight_matrix, undirected_graph)
     nodes[1].add_neighbour(nodes[3], 2, weight_matrix, undirected_graph)
     nodes[2].add_neighbour(nodes[3], 4, weight_matrix, undirected_graph)
     nodes[2].add_neighbour(nodes[4], 3, weight_matrix, undirected_graph)
-    nodes[3].add_neighbour(nodes[4], 2, weight_matrix, undirected_graph)
-    nodes[3].add_neighbour(nodes[5], 1, weight_matrix, undirected_graph)
-    nodes[4].add_neighbour(nodes[5], 6, weight_matrix, undirected_graph)
+    nodes[3].add_neighbour(nodes[4], 1, weight_matrix, undirected_graph)
+    nodes[3].add_neighbour(nodes[5], 10, weight_matrix, undirected_graph)
+    nodes[4].add_neighbour(nodes[5], 1, weight_matrix, undirected_graph)
+
     return nodes
 
 
@@ -40,8 +44,8 @@ def dijkstra(nodes):
                 possible = node.current_value + weight_matrix[node.id][current_neighbour.id]
                 if (possible < current_neighbour.current_value) | (current_neighbour.current_value == -1):
                     current_neighbour.current_value = possible
-                    new_optimus = node.optimus_path
-                    new_optimus.append(node.id)
+                    new_optimus = node.optimus_path.copy()
+                    new_optimus.append(current_neighbour.id)
                     current_neighbour.optimus_path = new_optimus
                     if debug:
                         print("Found new minimum of " + str(possible)
@@ -50,6 +54,7 @@ def dijkstra(nodes):
                 # we're on 1st node
                 current_neighbour.current_value = weight_matrix[node.id][current_neighbour.id]
                 current_neighbour.optimus_path.append(node.id)
+                current_neighbour.optimus_path.append(current_neighbour.id)
                 if debug:
                     print("Found new minimum of " + str(weight_matrix[node.id][current_neighbour.id]))
         node.completed = True
